f0c4e71b875fb1a3d1bccc5c6099541d2a5c1cb3
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-09-17 16:57:17 +0200

    Finish

--- statements.sql
+++ statements.sql
@@ -82,27 +82,85 @@ JOIN roles ON usuarios.id_rol = roles.id_rol;
 /* Relación tipo 1:N */
 -- PASO 1
 -- Tu código aquí
+CREATE TABLE id_categorias (
+id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+nombre_categoria VARCHAR(100) NOT NULL
+);
 
+INSERT INTO id_categorias (nombre_categoria) VALUES
+('Electrónicos'),
+('Ropa y Accesorios'),
+('Libros'),
+('Hogar y Cocina'),
+('Deportes y aire libre'),
+('Salud y cuidado personal'),
+('Herramientas y mejoras para el hogar'),
+('Juguetes y juegos'),
+('Automotriz'),
+('Música y Películas');
 
 -- PASO 2
 -- Tu código aquí
 
-
+ALTER TABLE usuarios ADD COLUMN id_categoria INT;
 -- PASO 3
 -- Tu código aquí
 
-
+UPDATE usuarios SET id_categoria = 1 WHERE id_usuario IN (1, 11);
+UPDATE usuarios SET id_categoria = 2 WHERE id_usuario IN (2, 12);
+UPDATE usuarios SET id_categoria = 3 WHERE id_usuario IN (3, 13);
+UPDATE usuarios SET id_categoria = 4 WHERE id_usuario IN (4, 14);
+UPDATE usuarios SET id_categoria = 5 WHERE id_usuario IN (5, 15);
+UPDATE usuarios SET id_categoria = 6 WHERE id_usuario IN (6, 16);
+UPDATE usuarios SET id_categoria = 7 WHERE id_usuario IN (7, 17);
+UPDATE usuarios SET id_categoria = 8 WHERE id_usuario IN (8, 18);
+UPDATE usuarios SET id_categoria = 9 WHERE id_usuario IN (9, 19);
+UPDATE usuarios SET id_categoria = 10 WHERE id_usuario IN (10, 20);
 -- PASO 4
 -- Tu código aquí
+
+SELECT 
+    usuarios.id_usuario, 
+    usuarios.nombre, 
+    usuarios.apellido, 
+    usuarios.email, 
+    usuarios.edad, 
+    roles.nombre_rol, 
+    categorias.nombre_categoria
+FROM usuarios
+JOIN roles ON usuarios.id_rol = roles.id_rol
+JOIN categorias ON usuarios.id_categoria = categorias.id_categoria;
 
 /* Relación tipo N:M */
 -- PASO 1
 -- Tu código aquí
-
+CREATE TABLE usuarios_categorias (
+id_usuario_categoria INT AUTO_INCREMENT PRIMARY KEY,
+id_usuario INT,
+id_categoria INT,
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
+); 
 
 -- PASO 2
 -- Tu código aquí
 
-
+INSERT INTO usuarios_categorias (id_usuario, id_categoria) VALUES
+(1, 1), (1, 2), (1, 3),
+(2, 4), (2, 5),
+(3, 6), (3, 7),
+(4, 8), (4, 9), (4, 10);
 -- PASO 3
 -- Tu código aquí
+SELECT 
+usuarios.id_usuario,
+ usuarios.nombre, 
+ usuarios.apellido,
+  usuarios.email, 
+  usuarios.edad, 
+  roles.nombre_rol, 
+  categorias.nombre_categoria
+FROM usuarios
+JOIN roles ON usuarios.id_usuario = roles.id_rol
+JOIN usuarios_categorias ON usuarios.id_usuario = usuarios_categorias.id_usuario
+JOIN categorias ON usuarios_categorias.id_categoria = categorias.id_categoria;
